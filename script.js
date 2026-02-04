84357ddbaab00e58723b3c09b92766ebf7ad2b93
Nuno <nunomarquees@gmail.com>
2026-02-04 16:31:32 +0000

    Esboço inicial ex galeria imagens

---
+++ script.js
@@ -0,0 +1,20 @@
+const galeria = document.querySelector(".galeria-imagens");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const btnFechar = document.querySelector(".fechar");
+
+//codigo para maximizar as imagens
+
+galeria.addEventListener("click", (e) => {
+    // if (e.target.tagName === "IMG") {
+    //     modal.style.display = "flex";
+    //     const imagemSource = e.target.Attribute("src");
+    //     modalImagem.src = imagemSource;
+    console.log(`Clickado`);
+    }
+);
+
+//perceber pq e q na consola no Inspect no html so regista quando clicko nas imagens (tipo esta bem
+//mas em teoria devia registar quando clicko na 'galeria toda' e so dps e que deveria registar quando clico nas imagens - mais a frente no codigo)
+
+
